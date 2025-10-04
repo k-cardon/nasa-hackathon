fccf89ae171023dd2b1da2527ca35a46917f934f
streetzy <kamik0212@gmail.com>
2025-10-05 01:37:25 +0200

    redesign and testing that backend works

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,4 @@
 import { get_weather_data } from '$lib/server/weather_data';
-import type { PageServerLoad } from './$types';
 
 const locationCoordinates: number[] = [57.7089, 11.9746];
 const params: string = 'PRECSNO,PRECTOTCORR,PSH,WS50M_RANGE,T2M,CLOUD_AMT_DAY';
@@ -18,7 +17,16 @@ const viewpoints: number[][] = [
 const location_request_url: string = `https://power.larc.nasa.gov/api/temporal/monthly/point?start=2020&end=2025&parameters=${params}&community=ag&longitude=${locationCoordinates[0]}&latitude=${locationCoordinates[1]}&format=JSON`;
 
 export const actions = {
-	default: async () => {
+	default: async ({ request }) => {
+		const form_data = await request.formData();
+		const location = form_data.get('location');
+		const sunny = form_data.get('Sunny'); // either null or on, so treat it as such
+		const rainy = form_data.get('Rainy');
+		const cloudy = form_data.get('Cloudy');
+		const windy = form_data.get('Windy');
+		const preferred_locations = form_data.get('preferred-locations');
+
 		console.log(await get_weather_data(location_request_url));
+		console.log(location, sunny, rainy, cloudy, windy, preferred_locations);
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,79 @@
 <script>
-    let location = 'Gothenburg'
-    let weatherOptions = [
-        {id: 1, text: 'Sunny'},
-        {id: 2, text: 'Cloudy'},
-        {id: 3, text: 'Windy'},
-        {id: 4, text: 'Snowy'}
-    ];
-    let preferred_weather = '';
+	let location = 'Gothenburg';
+	let weatherOptions = [
+		{ id: 1, text: 'Sunny' },
+		{ id: 2, text: 'Cloudy' },
+		{ id: 3, text: 'Rainy' },
+		{ id: 4, text: 'Snowy' }
+	];
+	let preferredWeather = $state('');
+	//let placesOfInterest = $state('');
+	//let date = $state('');
 </script>
 
-<div class="gap-4 w-full h-full items-center text-center justify-center p-5  ">
-    <div class="border-black border-2 border-solid rounded-lg">
-        <p class="my-5 mx-30 px-10 text-center border-black border-2 border-solid rounded-sm">Some URL ourwebsite.com/somewebsite</p>
-        <p class="mr-30 text-right">our mission</p>
-            <div class="border-black border-2 border-solid rounded-sm mb-5 mx-30">
-                <h1 class="flex text-3xl justify-center text-center pt-3">Welcome</h1>
-                <form>
-                    <p class="mx-5 p-5">What weather are you looking for today?</p>
-                        <div>
-                        {#each weatherOptions as weather}
-                            <button
-                                type="button"
-                                class="bg-gray-100 text-gray-800 hover:bg-emerald-200 hover:text-emerald-500 hover:border-emerald-500 font-semibold py-2 px-2 m-2 border-2 border-gray-400 rounded shadow"
-                                on:click={() => preferred_weather = weather.text}
-                                class:selected={preferred_weather === weather.text}>
-                                {weather.text}
-                            </button>
-                        {/each}
-                        </div>
-                    <div>
-                        <button class="text-lg bg-sky-200 border-sky-300 border-2 border-solid hover:bg-sky-300 text-sky-800 font-semibold m-4 p-2 rounded shadow w-1/2" disabled={!preferred_weather} type="submit"> Show locations </button>
-                    </div>
-                    <div>
-                        <span>Current location: </span><input class="m-5" bind:value={location} />
-                    </div>
-                </form>
-            </div>
-    </div>
+<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-5 text-center">
+	<div
+		class="align-center flex h-[75%] w-[60%] flex-col justify-center rounded-lg border-2 border-solid border-black"
+	>
+		<p class="mx-30 my-5 rounded-sm border-2 border-solid border-black px-10 text-center">
+			<!--Perhaps we could come up with a name for our app? In the theme of "Where Can/Shall I Go?" -->
+			Some URL ourwebsite.com/somewebsite
+		</p>
+		<p class="mr-30 text-right">our mission</p>
+		<div class="mx-30 mb-5 rounded-sm border-2 border-solid border-black">
+			<h1 class="flex justify-center pt-3 text-center text-3xl">Welcome</h1>
+			<form method="POST" class="flex flex-col gap-3">
+				<p class="mx-5">What weather are you looking for today?</p>
+				<div class="flex flex-row justify-center">
+					{#each weatherOptions as weather (weather.id)}
+						<div class="flex flex-row">
+							<input type="checkbox" id={weather.text} name={weather.text} class="peer hidden" />
+							<label
+								for={weather.text}
+								class="m-2 rounded border-2 border-gray-400 bg-gray-100 px-2 py-2 font-semibold text-gray-800 shadow peer-checked:border-emerald-500 hover:border-emerald-500 hover:bg-emerald-200 hover:text-emerald-500"
+							>
+								{weather.text}
+							</label>
+						</div>
+					{/each}
+				</div>
+
+				<div>
+					<label for="date">
+						<p>When would you like to go?</p>
+						<input class="w-1/3 rounded" type="date" name="date" />
+					</label>
+				</div>
+
+				<div>
+					<label for="preferred-locations">
+						<p>To which sorts of places would you like to go?</p>
+						<input
+							class="w-1/3 rounded"
+							name="preferred-locations"
+							type="text"
+							placeholder="Parks, concert venues..."
+						/>
+					</label>
+				</div>
+
+				<div>
+					<button
+						class="m-4 w-4/10 rounded border-2 border-solid border-sky-300 bg-sky-200 p-2 text-lg font-semibold text-sky-800 shadow hover:bg-sky-300"
+						type="submit"
+					>
+						Show locations
+					</button>
+				</div>
+
+				<div>
+					<span>Current location: </span>
+					<input name="location" class="m-2" bind:value={location} />
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
 
 <style>
-	li {
-		display: flex;
-	}
-
-	input[type="text"] {
-		flex: 1;
-		padding: 0.5em;
-		margin: -0.2em 0;
-		border: none;
-	}
 </style>
-
-

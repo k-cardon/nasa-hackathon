f4ac06a2639c87d0e7bf4a0b763c0a30e0335d3b
k-cardon <cardon.kristen.n@gmail.com>
2025-10-04 12:09:15 +0200

    basic frontend text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold center m-10 p-5">Will It Rain On My Parade?</h1>
+<p class="mx-10 p-5">NASA Hackathon 2025</p>
+<p class="mx-10 px-5">Which location would you like to search?</p>
+<p class="mx-10 px-5">What kind of weather are you looking for?</p>
+<ul class="mx-10 px-8"><li>Rain</li><li>Sunny</li><li>Cloudy</li><li>Wind</li><li>Snow</li></ul>
+<p class="mx-10 px-5">What kind of weather do you want to avoid?</p>
+<ul class="mx-10 px-8"><li>Rain</li><li>Sunny</li><li>Cloudy</li><li>Wind</li><li>Snow</li></ul>
+

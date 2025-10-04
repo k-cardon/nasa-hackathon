fbaf9db8a88174e82ae14ac4c7a0bdcf2d2b457d
k-cardon <cardon.kristen.n@gmail.com>
2025-10-04 12:15:50 +0200

    fix spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl font-bold center m-10 p-5">Will It Rain On My Parade?</h1>
+<h1 class="text-3xl font-bold center mx-10 mt-10 p-5">Will It Rain On My Parade?</h1>
 <p class="mx-10 p-5">NASA Hackathon 2025</p>
 <p class="mx-10 px-5">Which location would you like to search?</p>
 <p class="mx-10 px-5">What kind of weather are you looking for?</p>

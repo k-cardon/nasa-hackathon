f3d1c4a0fdc32f8fa9b32e7ae85277aca46327c5
aaravalanche <gusmajuaa@student.gu.se>
2025-10-04 16:40:40 +0200

    fixed velte problem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,11 @@
 
     let avoid_weather = '';
     let preferred_weather = '';
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        alert(`you are looking for ${preferred_weather} and you want to avoid ${avoid_weather} in ${location}`)
-    }    
 </script>
 
 <h1 class="text-3xl font-bold center mx-10 mt-10 p-5">Will It Rain On My Parade?</h1>
 <p class="mx-10 p-5">NASA Hackathon 2025</p>
-<form onsubmit={handleSubmit}>
+<form>
     <p class="mx-10 px-5">Which location would you like to search?</p>
     <input class="mx-15 px-5" bind:value={location} />
 
@@ -40,68 +35,7 @@
     <button disabled={!preferred_weather} type="submit"> Submit </button>
 </form>
 
-<script>
-	let todos = $state([
-		{ done: false, text: 'finish Svelte tutorial' },
-		{ done: false, text: 'build an app' },
-		{ done: false, text: 'world domination' }
-	]);
-
-	function add() {
-		todos.push({
-			done: false,
-			text: ''
-		});
-	}
-
-	function clear() {
-		todos = todos.filter((t) => !t.done);
-	}
-
-	let remaining = $derived(todos.filter((t) => !t.done).length);
-</script>
-
-<div class="centered">
-	<h1>todos</h1>
-
-	<ul class="todos">
-		{#each todos as todo}
-			<li class={{ done: todo.done }}>
-				<input
-					type="checkbox"
-					checked={todo.done}
-				/>
-
-				<input
-					type="text"
-					placeholder="What needs to be done?"
-					value={todo.text}
-				/>
-			</li>
-		{/each}
-	</ul>
-
-	<p>{remaining} remaining</p>
-
-	<button onclick={add}>
-		Add new
-	</button>
-
-	<button onclick={clear}>
-		Clear completed
-	</button>
-</div>
-
 <style>
-	.centered {
-		max-width: 20em;
-		margin: 0 auto;
-	}
-
-	.done {
-		opacity: 0.4;
-	}
-
 	li {
 		display: flex;
 	}

75787ee2fee3c47ebee713b6eb4ddc46841165a8
k-cardon <cardon.kristen.n@gmail.com>
2025-10-04 12:11:13 +0200

    update weather categories

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <p class="mx-10 p-5">NASA Hackathon 2025</p>
 <p class="mx-10 px-5">Which location would you like to search?</p>
 <p class="mx-10 px-5">What kind of weather are you looking for?</p>
-<ul class="mx-10 px-8"><li>Rain</li><li>Sunny</li><li>Cloudy</li><li>Wind</li><li>Snow</li></ul>
+<ul class="mx-10 px-8"><li>Temperature</li><li>Precipitation</li><li>Sun</li></ul>
 <p class="mx-10 px-5">What kind of weather do you want to avoid?</p>
-<ul class="mx-10 px-8"><li>Rain</li><li>Sunny</li><li>Cloudy</li><li>Wind</li><li>Snow</li></ul>
+<ul class="mx-10 px-8"><li>Temperature</li><li>Precipitation</li><li>Sun</li></ul>
 

8e74822520ccc1121746813ca794159b5c2b91ea
aaravalanche <gusmajuaa@student.gu.se>
2025-10-05 11:36:11 +0200

    Fixed tailwind import errors and created rudimentary Dropdown and Alert

--- src/routes/aarav/+page.svelte
+++ src/routes/aarav/+page.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
-  import { Alert } from "flowbite-svelte";
-  import { InfoCircleSolid } from "flowbite-svelte-icons";
+  import { Alert, Button, Dropdown, DropdownItem, DropdownGroup, DropdownHeader } from "flowbite-svelte";
+  import { blur } from "svelte/transition"
+  import { ChevronDownOutline, InfoCircleSolid } from "flowbite-svelte-icons";
+	import type { svelte } from "@sveltejs/vite-plugin-svelte";
+	import { cubicInOut } from "svelte/easing";
 </script>
 
-<style>
-  .alert {
-    margin: 50%;
-    padding-left: 60%;
+<style></style>
 
-  }
-</style>
-
-
-<Alert color="red" class="alert">
-  {#snippet icon()}<InfoCircleSolid class="h-5 w-5"/>{/snippet}
-  <span class="font-bold">Under development!</span>
-  More features coming soon!
+<Alert
+  color="amber"
+  border
+  dismissable
+  transition={blur}
+  params={{duration:500,easing:cubicInOut}}
+>
+  {#snippet icon()}
+    <InfoCircleSolid class="h-5 w-5" />
+  {/snippet}
+  Under maintenance! This is just a demo of "Name"
 </Alert>
+
+
+<Button>Choose a location<ChevronDownOutline class="ms-2 h-6 w-6 text-gray dark:text-black" /></Button>
+<Dropdown>
+  <DropdownHeader>
+    <span class="block truncate text-sm font-medium">More locations coming soon!</span>
+  </DropdownHeader>
+  <DropdownGroup>
+    <DropdownItem>Gothenburg</DropdownItem>
+  </DropdownGroup>
+</Dropdown>
+

e04e87b6b675be2325f20957bacac00aeeee5ef9
k-cardon <cardon.kristen.n@gmail.com>
2025-10-05 10:52:51 +0200

    mobile design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,12 @@
 	//let date = $state('');
 </script>
 
-<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-5 text-center">
+<div class="flex h-full w-full flex-col items-center justify-center gap-4 text-center">
 	<div
-		class="align-center flex h-[75%] w-[60%] flex-col justify-center rounded-lg"
+		class="align-center flex md:h-[75%] md:w-[60%] flex-col justify-center rounded-lg"
 	>
 		<p class="mr-30 text-right">our mission</p>
-		<div class="mx-30 mb-5 rounded-sm border-2 border-solid border-black">
+		<div class="md:mx-30 mb-5 rounded-sm border-2 border-solid border-black">
 			<h1 class="flex justify-center pt-3 text-center text-3xl">Welcome</h1>
 			<form method="POST" class="flex flex-col gap-3">
 				<p class="mx-5">What weather are you looking for today?</p>
@@ -37,32 +37,32 @@
 				<div>
 					<label for="date">
 						<p>When would you like to go?</p>
-						<input class="w-1/3 rounded" type="date" name="date" />
+						<input class="w-1/2 rounded" type="date" name="date" />
 					</label>
 				</div>
 
 				<div>
 					<label for="preferred-locations">
-						<p>To which sorts of places would you like to go?</p>
+						<p>Where would you like to go?</p>
 						<input
-							class="w-1/3 rounded"
+							class="w-5/6 rounded"
 							name="preferred-locations"
 							type="text"
-							placeholder="Parks, concert venues..."
+							placeholder="Parks, concert venues, beaches..."
 						/>
 					</label>
 				</div>
 
 				<div>
 					<button
-						class="m-4 w-4/10 rounded border-2 border-solid border-sky-300 bg-sky-200 p-2 text-lg font-semibold text-sky-800 shadow hover:bg-sky-300"
+						class="m-4 w-1/2 rounded border-2 border-solid border-sky-300 bg-sky-200 p-2 text-lg font-semibold text-sky-800 shadow hover:bg-sky-300"
 						type="submit"
 					>
 						Show locations
 					</button>
 				</div>
 
-				<div>
+				<div class="p-8 md:p-0">
 					<span>Current location: </span>
 					<input name="location" class="m-2" bind:value={location} />
 				</div>

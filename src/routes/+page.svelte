c90cdeb1163eed564679b9a3350c7e62111c1618
k-cardon <cardon.kristen.n@gmail.com>
2025-10-05 17:14:37 +0200

    add results to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,8 @@
 	let preferredWeather = $state('');
 
 	let { form }: PageProps = $props();
+    let showResult = $state(false);
+    let searchAgain = $state(false);
 	//let placesOfInterest = $state('');
 	//let date = $state('');
 </script>
@@ -17,7 +19,7 @@
 {#if !form?.success}
 	<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-5 text-center">
 		<div
-			class="align-center flex h-[75%] w-[60%] flex-col justify-center rounded-lg border-2 border-solid border-black"
+			class="align-center flex h-[75%] w-[60%] flex-col justify-center rounded-lg"
 		>
 			<p class="mr-30 text-right">our mission</p>
 			<div class="mx-30 mb-5 rounded-sm border-2 border-solid border-black">
@@ -41,15 +43,15 @@
 					<div>
 						<label for="date">
 							<p>When would you like to go?</p>
-							<input class="w-1/3 rounded" type="date" name="date" required />
+							<input class="w-1/2 rounded" type="date" name="date" required />
 						</label>
 					</div>
 
 					<div>
 						<label for="preferred-locations">
-							<p>To which sorts of places would you like to go?</p>
+							<p>Where would you like to go?</p>
 							<input
-								class="w-1/3 rounded"
+								class="w-5/6 rounded"
 								name="preferred-locations"
 								type="text"
 								placeholder="Gothenburg, Malmo..."
@@ -59,7 +61,7 @@
 
 					<div>
 						<button
-							class="m-4 w-4/10 rounded border-2 border-solid border-sky-300 bg-sky-200 p-2 text-lg font-semibold text-sky-800 shadow hover:bg-sky-300"
+							class="m-4 w-1/2 rounded border-2 border-solid border-sky-300 bg-sky-200 p-2 text-lg font-semibold text-sky-800 shadow hover:bg-sky-300"
 							type="submit"
 						>
 							Show locations
@@ -67,7 +69,7 @@
 					</div>
 
 					<div>
-						<span>Current location: </span>
+						<span class="p-8 md:p-0">Current location: </span>
 						<input name="location" class="m-2" bind:value={location} />
 					</div>
 				</form>
@@ -75,9 +77,13 @@
 		</div>
 	</div>
 {:else}
-	{#each form.ranked_cities as city (city)}
-		<p>{city[0]} {city[1]} {city[2]}</p>
-	{/each}
+        <div class="flex h-full w-full flex-col items-center justify-center gap-4 p-5 text-center">
+                {#each form.ranked_cities as city (city)}
+                        <div class="flex h-1/3 w-1/2 flex-col items-center justify-center gap-4 p-5 text-center md:mx-30 mb-5 rounded-sm border-2 border-solid border-black">
+                <p>{city[0]} {city[1]} {city[2]}</p></div>
+            {/each}
+            <a href="/">Search again?</a>
+        </div>
 {/if}
 
 <style>

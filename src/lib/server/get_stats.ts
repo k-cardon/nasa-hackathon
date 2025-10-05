5d18466575e0ddfa769b8901fc329c7dd7261f4a
Aurora-A <kimjoviken@hotmail.com>
2025-10-05 08:55:45 +0200

    Moved to server

---
+++ src/lib/server/get_stats.ts
@@ -0,0 +1,62 @@
+import dater from '$lib/assets/message.json';
+function calculate_average() {
+
+
+    //PLACEHOLDER VARIABLES
+    //To be passed by the form/DOM to find which stat is requested.
+    let entered_stat = "Precipitation"
+    let entered_month = "November";
+    
+    
+    function get_search_strings(month: string, stat: string) {
+        let months = {
+            'January'  : "01",
+            'February' : "02",
+            'March'    : "03",
+            'April'    : "04",
+            'May'      : "05",
+            'June'     : "06",
+            'July'     : "07",
+            'August'   : "08",
+            'September': "09",
+            'October'  : "10",
+            'November' : "11",
+            'December' : "12",
+        }
+        let stats = {
+            'Temperature' : "???",
+            'Precipitation' : "PRECTOTCORR",
+            'Cloudiness' : 'CLOUD_AMT_DAY'
+        }
+        return [months[month], stats[stat]];
+    };
+
+    let [search_month, search_stat] = get_search_strings(entered_month, entered_stat);
+    console.log(search_month, search_stat);
+
+    let avg_stat = 0;
+    let vaild_entries = 0;
+    let search_data = dater[search_stat];
+
+    Object.entries(search_data).forEach((month) => {
+        if(search_month == month[0].slice(4, 6)){
+            if(month[1] != (-999)) {
+                console.log(month[1]);
+                vaild_entries++;
+                avg_stat += month[1]
+            }
+        }
+    });
+
+    avg_stat = (avg_stat / vaild_entries);
+    console.log("AVERAGE STAT: " + avg_stat);
+    //Do we want to return a value or directly modify the DOM?
+    //console.log("Average cloudiness in " + entered_month + " is " + avg_cloudiness + "%");
+
+
+    //--------------------------------------------------------------
+    //new stuff
+
+
+
+}
